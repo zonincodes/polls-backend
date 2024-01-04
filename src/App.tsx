8258f3237d52739495ec34a6807f89d4c97c31b2
Zonin <business.kariukimartin@gmail.com>
2024-01-04 20:16:53 +0300

    local storage theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Hero from './components/Hero';
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Hero from "./components/Hero";
 
 function App() {
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+
+  });
   return (
     <div>
-      <Hero/>
+      <Hero />
     </div>
   );
 }
